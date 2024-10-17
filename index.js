807eec9bd2637e5d7d9434cb5c5cbe988b09ca00
Himanshu Parashar <parasharhimanshu41@gmail.com>
2024-10-17 16:31:31 +0530

    Poems

---
+++ index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();  // Load environment variables from .env file
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+
+// bot.on('message', (option) => {
+//     console.log("Message received on the bot:", option.text); // Log the message text
+
+//     bot.sendMessage(option.chat.id, "Hello").catch((error) => {
+//         console.error("Error sending message:", error); // Handle potential errors
+//     });
+// });
+
+bot.on('message', (option) => {
+        bot.sendMessage(option.chat.id, "Hello, type /hi to get an exclusive shayri...").catch((error) => {
+            console.error("Error sending message:", error); // Handle potential errors
+        });
+    });
+
+
+bot.onText(/\/hii/, async (option) => {
+    const response = await axios.get('http://localhost:3000/shayri');
+    const poems = response.data;
+
+    console.log(option);
+
+    const randomIndex = Math.floor(Math.random() * poems.length);
+    const poem = poems[randomIndex];
+
+    bot.sendMessage(option.chat.id, poem.text);
+})
